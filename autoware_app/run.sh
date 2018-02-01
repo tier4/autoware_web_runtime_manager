75f69569e6ba2e6db1663583bb655bc1c0f408be
yabuta <tefutefu1989@gmail.com>
2018-02-01 13:56:20 +0900

    Fix run.sh

--- autoware_app/run.sh
+++ autoware_app/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python ./prepare.py
-bash -l -c 'source ~/Autoware/ros/devel/setup.bash' &
+source ~/Autoware/ros/devel/setup.bash
 python ./router.py
 sleep 2
 # x-www-browser http://localhost:5000/
