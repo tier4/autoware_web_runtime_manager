7848d088a78aac7fa5d3f7c3f4d7f37e2bca9b57
yabuta <tefutefu1989@gmail.com>
2018-02-01 15:50:34 +0900

    Add sample.env in app/config Delete unnessesary comment

--- app/run.sh
+++ app/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 python ./prepare.py
-bash -l -c 'source ../../ros/devel/setup.bash; python ./router.py' &
+bash -l -c 'python ./router.py' &
 sleep 2
 # x-www-browser http://localhost:5000/
