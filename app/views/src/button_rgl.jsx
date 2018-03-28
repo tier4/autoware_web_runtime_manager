afafa5a7d5ead8df7af56b988920eb2933d9cb7c
yabuta <tefutefu1989@gmail.com>
2018-03-28 19:13:40 +0900

    Fix where pointed out pull request comment

--- app/views/src/button_rgl.jsx
+++ app/views/src/button_rgl.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Responsive, { WidthProvider } from 'react-grid-layout';
+import Responsive, {WidthProvider} from 'react-grid-layout';
+
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 import Button from "./button";
 import ROSLaunchRequest from "./roslaunch_request";
@@ -11,7 +12,7 @@ class GridSizeWrapper extends React.Component {
         const that = this;
         const newChildren = React.Children.map(
             this.props.children,
-            function(child) {
+            function (child) {
                 return React.cloneElement(
                     child,
                     {
@@ -35,7 +36,7 @@ class GridSizeWrapper extends React.Component {
 export default class ButtonRGL extends React.Component {
     getLayout() {
         const layout = [];
-        for(const node of this.props.structure.nodes) {
+        for (const node of this.props.structure.nodes) {
             layout.push(
                 {
                     i: node.id + "",
@@ -43,28 +44,30 @@ export default class ButtonRGL extends React.Component {
                     y: node.y,
                     w: node.w,
                     h: node.h,
-                    static: true}
+                    static: true
+                }
             );
         }
         return layout;
     }
+
     getButtons() {
         const buttons = [];
-        for(const node of this.props.structure.nodes) {
+        for (const node of this.props.structure.nodes) {
             let buttonState = null;
-            if(node.on) {
+            if (node.on) {
                 buttonState = "on";
             }
-            else{
+            else {
                 buttonState = "off";
             }
-            if(node.isLoading) {
+            if (node.isLoading) {
                 buttonState = "disabled";
             }
-            if(node.isKilling) {
+            if (node.isKilling) {
                 buttonState = "disabled";
             }
-            if(!node.enabled) {
+            if (!node.enabled) {
                 buttonState = "disabled";
             }
             buttons.push((
@@ -79,32 +82,33 @@ export default class ButtonRGL extends React.Component {
         }
         return buttons;
     }
+
     componentWillMount() {
         this.initializeButtonRGLState();
 
-	//mqtt callback method creating
-	const mqttClient = this.props.mqttClient;
-	
-	var buttonMethod = function(message){
-	    //console.log(message.payloadString);
-	    const topic_factor = message.destinationName.split("/");
-	    const message_factor = topic_factor[2].split(".");
-            const index = this.props.structure.nodes.findIndex(node => node.label === message_factor[2]);
-	    //console.log(index);
-	    
-	    if(message.payloadString === "ok"){
-		this.props.structure.nodes[index].span = (<span>{this.props.structure.nodes[index].display}</span>);
-	    }else{
-		this.props.structure.nodes[index].span = (<span>error</span>);
-	    }
-	    this.props.updateStructure(this.props.structure);
-	};
+        //mqtt callback method creating
+        const mqttClient = this.props.mqttClient;
 
-	for(const node of this.props.structure.nodes){
-	    mqttClient.setCallback(node.label,buttonMethod.bind(this));
-	}
+        var buttonMethod = function (message) {
+            //console.log(message.payloadString);
+            const topic_factor = message.destinationName.split("/");
+            const message_factor = topic_factor[2].split(".");
+            const index = this.props.structure.nodes.findIndex(node => node.label === message_factor[2]);
+            //console.log(index);
+
+            if (message.payloadString === "ok") {
+                this.props.structure.nodes[index].span = (<span>{this.props.structure.nodes[index].display}</span>);
+            } else {
+                this.props.structure.nodes[index].span = (<span>error</span>);
+            }
+            this.props.updateStructure(this.props.structure);
+        };
+
+        for (const node of this.props.structure.nodes) {
+            mqttClient.setCallback(node.label, buttonMethod.bind(this));
+        }
     }
-    
+
     render() {
         return (
             <ResponsiveReactGridLayout
@@ -117,42 +121,48 @@ export default class ButtonRGL extends React.Component {
             </ResponsiveReactGridLayout>
         );
     }
+
     initializeButtonRGLState() {
         //const date = new Date();
-	//todo:change mqtt
+        //todo:change mqtt
         //const url = WEB_UI_URL+"/getRTMStatus?date="+date.getTime().toString();
 
-	var initMethod = function(message){
-	    //console.log(message.payloadString);
-	    const json = JSON.parse(message.payloadString);
+        var initMethod = function (message) {
+            //console.log(message.payloadString);
+            const json = JSON.parse(message.payloadString);
             //console.log("initializeButtonRGLState", json);
             const structure = this.props.structure;
-            for(const topic_name of Object.keys(json)){
-		//console.log("topic:" + json[topic_name]);
-                const index = this.props.structure.nodes.findIndex(function(x) { return x.label === topic_name; });
-		if(index !== -1){
-		    structure.nodes[index].enabled = json[topic_name]["enable"];
-		}
+            for (const topic_name of Object.keys(json)) {
+                //console.log("topic:" + json[topic_name]);
+                const index = this.props.structure.nodes.findIndex(function (x) {
+                    return x.label === topic_name;
+                });
+                if (index !== -1) {
+                    structure.nodes[index].enabled = json[topic_name]["enable"];
+                }
             }
-            for(const topic_name of Object.keys(json)){
-                const index = this.props.structure.nodes.findIndex(function(x) { return x.label === topic_name; });
-		if(index !== -1){
-                    if(json[topic_name]["mode"]=="on" && index !== -1){
-			structure.nodes = this.getUpdatedNodes(
+            for (const topic_name of Object.keys(json)) {
+                const index = this.props.structure.nodes.findIndex(function (x) {
+                    return x.label === topic_name;
+                });
+                if (index !== -1) {
+                    if (json[topic_name]["mode"] == "on" && index !== -1) {
+                        structure.nodes = this.getUpdatedNodes(
                             this.props.structure.nodes[index].id,
                             !this.props.structure.nodes[index].on,
-			    this.props.structure.nodes);
+                            this.props.structure.nodes);
                     }
-		}
-            }        
+                }
+            }
             this.props.updateStructure(structure);
         };
-	this.props.mqttClient.setCallback("buttonInit",initMethod.bind(this));
-		
+        this.props.mqttClient.setCallback("buttonInit", initMethod.bind(this));
+
     }
+
     onClickButton(nodeID) {
         const index = this.props.structure.nodes.findIndex(node => node.id === nodeID);
-        if(this.props.structure.nodes[index].enabled) {
+        if (this.props.structure.nodes[index].enabled) {
             const structure = this.props.structure;
             structure.nodes = this.getUpdatedNodes(
                 nodeID,
@@ -162,32 +172,32 @@ export default class ButtonRGL extends React.Component {
             const nodeDomain = structure.nodes[index].domain;
             const nodeLabel = structure.nodes[index].label;
             const nodeDisplay = structure.nodes[index].display;
-            const url = WEB_UI_URL+"/roslaunch/"+nodeDomain+"/"+nodeLabel+"/"+nodeID+"/"+(this.props.structure.nodes[index].on ? "on" : "off");
+            const url = WEB_UI_URL + "/roslaunch/" + nodeDomain + "/" + nodeLabel + "/" + nodeID + "/" + (this.props.structure.nodes[index].on ? "on" : "off");
 
-	    /*
-            structure.nodes[index].span = (<ROSLaunchRequest
-                url={url}
-                errorCallback={() => { return (<span>error</span>); }}
-                isLoadingCallback={() => {
-                    return (<span>{(this.props.structure.nodes[index].on ? "loading.." : "killing..")}</span>); }
-                }
-                responseCallback={() => {
-                    if(index==0 && !this.props.structure.nodes[index].on) {
-                        location.reload();
+            /*
+                structure.nodes[index].span = (<ROSLaunchRequest
+                    url={url}
+                    errorCallback={() => { return (<span>error</span>); }}
+                    isLoadingCallback={() => {
+                        return (<span>{(this.props.structure.nodes[index].on ? "loading.." : "killing..")}</span>); }
                     }
-                    return (<span>{nodeDisplay}</span>);
-                }}
-                defaultCallback={() => { return (<span>{nodeDisplay}</span>); }}
-					   />);
-	    */
+                    responseCallback={() => {
+                        if(index==0 && !this.props.structure.nodes[index].on) {
+                            location.reload();
+                        }
+                        return (<span>{nodeDisplay}</span>);
+                    }}
+                    defaultCallback={() => { return (<span>{nodeDisplay}</span>); }}
+                           />);
+            */
 
-	    // set callback handlers
-	    const label = structure.nodes[index].label;
-	    const message = structure.nodes[index].on ? "on" : "off";
-	    this.props.mqttClient.onPublish(label,message);
-	    structure.nodes[index].span = (<span>{(structure.nodes[index].on ? "loading.." : "killing..")}</span>);
+            // set callback handlers
+            const label = structure.nodes[index].label;
+            const message = structure.nodes[index].on ? "on" : "off";
+            this.props.mqttClient.onPublish(label, message);
+            structure.nodes[index].span = (<span>{(structure.nodes[index].on ? "loading.." : "killing..")}</span>);
 
-	    
+
             /*
             let that = this;
             const xhttpRequest = new XMLHttpRequest();
@@ -214,38 +224,39 @@ export default class ButtonRGL extends React.Component {
             this.props.updateStructure(structure);
         }
     }
+
     getUpdatedNodes(nodeID, on, nodes) {
         const index = nodes.findIndex(node => node.id === nodeID);
         nodes[index].on = on;
         let changeFlag = true;
-        while(changeFlag) {
-            changeFlag=false;
-            for(const nodeIndex in nodes) {
+        while (changeFlag) {
+            changeFlag = false;
+            for (const nodeIndex in nodes) {
                 const node = nodes[nodeIndex];
                 let enabled = true;
-                for(const requiredNodeID of node.required.forEnable.off) {
-                    if(nodes[nodes.findIndex(node => node.id === requiredNodeID)].on) {
+                for (const requiredNodeID of node.required.forEnable.off) {
+                    if (nodes[nodes.findIndex(node => node.id === requiredNodeID)].on) {
                         enabled = false;
                         break;
                     }
                 }
-                if(enabled) {
-                    for(const requiredNodeID of node.required.forEnable.disable) {
-                        if(nodes[nodes.findIndex(node => node.id === requiredNodeID)].enabled) {
+                if (enabled) {
+                    for (const requiredNodeID of node.required.forEnable.disable) {
+                        if (nodes[nodes.findIndex(node => node.id === requiredNodeID)].enabled) {
                             enabled = false;
                             break;
                         }
                     }
-                    if(enabled) {
-                        for(const requiredNodeID of node.required.forEnable.enable) {
-                            if(!nodes[nodes.findIndex(node => node.id === requiredNodeID)].enabled) {
+                    if (enabled) {
+                        for (const requiredNodeID of node.required.forEnable.enable) {
+                            if (!nodes[nodes.findIndex(node => node.id === requiredNodeID)].enabled) {
                                 enabled = false;
                                 break;
                             }
                         }
-                        if(enabled) {
-                            for(const requiredNodeID of node.required.forEnable.on) {
-                                if(!nodes[nodes.findIndex(node => node.id === requiredNodeID)].on) {
+                        if (enabled) {
+                            for (const requiredNodeID of node.required.forEnable.on) {
+                                if (!nodes[nodes.findIndex(node => node.id === requiredNodeID)].on) {
                                     enabled = false;
                                     break;
                                 }
@@ -253,9 +264,9 @@ export default class ButtonRGL extends React.Component {
                         }
                     }
                 }
-                if(node.enabled != enabled) {
+                if (node.enabled != enabled) {
                     nodes[nodeIndex].enabled = enabled;
-                    changeFlag=true;
+                    changeFlag = true;
                     break;
                 }
             }
--- app/views/src/dotenv.jsx
+++ app/views/src/dotenv.jsx
@@ -1,4 +1,7 @@
-import {AUTOWARE_WEB_UI_HOST, AUTOWARE_WEB_UI_PORT,MQTT_HOST,MQTT_JS_PORT} from "react-native-dotenv";
+import {MQTT_HOST,MQTT_JS_PORT} from "react-native-dotenv";
+
+const AUTOWARE_WEB_UI_HOST = location.hostname;
+const AUTOWARE_WEB_UI_PORT = location.port;
 
 const WEB_UI_URL = "http://"+AUTOWARE_WEB_UI_HOST+":"+AUTOWARE_WEB_UI_PORT;
 const MQTT_HOST_NAME = MQTT_HOST;
